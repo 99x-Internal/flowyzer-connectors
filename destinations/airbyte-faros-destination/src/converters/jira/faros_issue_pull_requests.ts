9b35d73bc5d2b2aed1c6f188242ea9bc44d14569
jaiprashanth99x <jaiprashanthr@99x.io>
2024-06-12 10:25:49 +0800

    Added Origin to PullRequest to easier data segregation

--- destinations/airbyte-faros-destination/src/converters/jira/faros_issue_pull_requests.ts
+++ destinations/airbyte-faros-destination/src/converters/jira/faros_issue_pull_requests.ts
@@ -56,6 +56,7 @@ export class FarosIssuePullRequests extends JiraConverter {
               name: toLower(pullRequest.repo.name),
             },
             number: pullRequest.number,
+            origin: 'jira',
           },
         },
       },
